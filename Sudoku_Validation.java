dee5c78fb93c9ae484c96ca00a8f706027e52f7e
TischTasche <soeren-petersen@gmx.net>
2025-03-19 17:14:26 +0800

    "Made functional"

--- Sudoku_Validation.java
+++ Sudoku_Validation.java
@@ -1,69 +1,97 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
-public class Sudoku_Validation{
-    public static void main(String[] Args){
-        System.out.println("Enter a sudoku puzzle to be checked:");
-        int[][] sudoku = createArray();
-        System.out.println("The given sudoku is: " + isValid(sudoku));
+public class Sudoku_Validation {
+    public static void main(String[] Args) {
+        System.out.println("Enter Sudoku puzzles separated by two consecutive line breaks. Press Enter twice to finish input:");
+        List<int[][]> sudokuPuzzles = createArrays(readInput());
+
+        for (int i = 0; i < sudokuPuzzles.size(); i++) {
+            System.out.println("The given Sudoku puzzle " + (i + 1) + " is: " + (isValid(sudokuPuzzles.get(i)) ? "Valid" : "Invalid"));
+        }
     }
 
-    public static int[][] createArray(){
+    public static String readInput() {
         Scanner scanner = new Scanner(System.in);
-        String[] inputString = new String[9];
-        for(int i = 0; i < 9; i++){
-            inputString[i] = scanner.nextLine();
-        }
+        String input = new String();
+        int emptyLineCount = 0;
 
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.isEmpty()) {
+                emptyLineCount++;
+                if (emptyLineCount == 2) {
+                    break;
+                }
+                input += "\n";
+            } else {
+                emptyLineCount = 0;
+                input += line + "\n";
+            }
+        }
+        scanner.close();
+        return input.toString();
+    }
+
+    public static List<int[][]> createArrays(String input) {
+        String[] sudokuStrings = input.split("\n\n");
+        List<int[][]> sudokuPuzzles = new ArrayList<>();
         int sudokuLength = 9;
-        int[][] sudoku = new int[sudokuLength][sudokuLength];
-        for(int i = 0; i < sudokuLength; i++){
-            if(inputString[i].length() != sudokuLength){
+        for (String sudokuString : sudokuStrings) {
+            String[] lines = sudokuString.split("\n");
+            int[][] sudoku = new int[sudokuLength][sudokuLength];
+
+            if (lines.length != sudokuLength) {
                 System.out.println("Invalid input");
                 System.exit(0);
             }
-            for(int j = 0; j < sudokuLength; j++){
-                sudoku[i][j] = Character.getNumericValue(inputString[i].charAt(j));
+
+            for (int i = 0; i < sudokuLength; i++) {
+                if (lines[i].length() != sudokuLength) {
+                    System.out.println("Invalid input");
+                    System.exit(0);
+                }
+                for (int j = 0; j < sudokuLength; j++) {
+                    sudoku[i][j] = Character.getNumericValue(lines[i].charAt(j));
+                }
             }
+            sudokuPuzzles.add(sudoku);
         }
-        scanner.close(); 
-        return sudoku;
+        return sudokuPuzzles;
     }
 
-    public static boolean isValid(int[][] sudoku){
-        if(checkRow(sudoku) && checkColumn(sudoku) && checkSubGrid(sudoku)){
-            return true;
-        }
-        return false;
+    public static boolean isValid(int[][] sudoku) {
+        return checkRow(sudoku) && checkColumn(sudoku) && checkSubGrid(sudoku);
     }
 
-
-    public static boolean checkRow(int[][]sudoku){
+    public static boolean checkRow(int[][] sudoku) {
         int sudokuLength = sudoku[0].length;
-        for(int i = 0; i < sudokuLength; i++){
+        for (int i = 0; i < sudokuLength; i++) {
             boolean[] seen = new boolean[sudokuLength + 1];
-            for(int j = 0; j < sudokuLength; j++){
+            for (int j = 0; j < sudokuLength; j++) {
                 int currentNum = sudoku[i][j];
-                if(currentNum < 1 || currentNum > 9 || seen[currentNum]) return false;
+                if (currentNum < 1 || currentNum > 9 || seen[currentNum]) return false;
                 seen[currentNum] = true;
             }
         }
         return true;
     }
 
-    public static boolean checkColumn(int[][]sudoku){
+    public static boolean checkColumn(int[][] sudoku) {
         int sudokuLength = sudoku[0].length;
-        for(int i = 0; i < sudokuLength; i++){
+        for (int i = 0; i < sudokuLength; i++) {
             boolean[] seen = new boolean[sudokuLength + 1];
-            for(int j = 0; j < sudokuLength; j++){
+            for (int j = 0; j < sudokuLength; j++) {
                 int currentNum = sudoku[j][i];
-                if(currentNum < 1 || currentNum > 9 || seen[currentNum]) return false;
+                if (currentNum < 1 || currentNum > 9 || seen[currentNum]) return false;
                 seen[currentNum] = true;
             }
         }
         return true;
     }
 
-    public static boolean checkSubGrid(int[][]sudoku){
+    public static boolean checkSubGrid(int[][] sudoku) {
         int sudokuLength = sudoku.length;
         int subGridSize = 3;
         for (int row = 0; row < sudokuLength; row += subGridSize) {
